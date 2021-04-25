a0161158c39c7841f834c6e16bd1b1ed96b1e71f
tmdwhd0123 <>
2021-04-25 11:18:11 +0000

    box sizing ing

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-banner', {
+      spaceBetween: 30,
+      effect: 'fade',
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    
