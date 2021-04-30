b73949cb896094e935a87d0afa094bda259d06e8
tmdwhd0123 <>
2021-04-30 08:19:01 +0000

    nav scroll

--- js/main.js
+++ js/main.js
@@ -449,3 +449,28 @@ $(document).ready(function () {
 
 
 });
+
+// Handle scrolling when tapping on the navbar menu
+function scrollToTargetAdjusted(a) {
+  var element = document.getElementById(a);
+  if (window.innerWidth < 768) { var headerOffset = 303 } else { var headerOffset = 63; };
+  var elementPosition = element.getBoundingClientRect().top;
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var offsetPosition = elementPosition + scrollTop - headerOffset;
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
+
+const navbarMenu = document.querySelector('.header__nav');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const linkid = document.querySelector(link).id;
+  scrollToTargetAdjusted(linkid);
+  navbarMenu.classList.remove('open');
+});
