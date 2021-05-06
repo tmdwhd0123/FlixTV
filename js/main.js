a233a9fd19f2b42d6ababdb3a43a9f98b114dddc
tmdwhd0123 <>
2021-05-06 14:41:23 +0000

    template video item 16:9 bootstrap m

--- js/main.js
+++ js/main.js
@@ -502,3 +502,29 @@ $(document).ready(function() {
     }]
   });
 });
+
+/*=============================================
+	=    		Isotope	Active  	      =
+=============================================*/
+$('.tr-movie-active').imagesLoaded(function () {
+	// init Isotope
+	var $grid = $('.tr-movie-active').isotope({
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-sizer',
+		}
+	});
+	// filter items on button click
+	$('.tr-movie-menu-active').on('click', 'button', function () {
+		var filterValue = $(this).attr('data-filter');
+		$grid.isotope({ filter: filterValue });
+	});
+
+});
+//for menu active class
+$('.tr-movie-menu-active button').on('click', function (event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
